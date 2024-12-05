c9a0927d2d4b7c4aa8aa4b9db4d7427b31d52886
Dinith Maleesha <maleesha.live123@gmail.com>
2024-12-05 11:36:51 +0530

    feat: clear notifications before showing a new one

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.runtime.onInstalled.addListener(() => {
-    const intervel = 45 // Change this value to adjust the reminder interval
+    const intervel = 1 // Change this value to adjust the reminder interval
     const now = new Date();
 
     console.log(`Hydrate Buddy installed and running! ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`);
@@ -24,15 +24,20 @@ function sendNotification() {
     const seconds = now.getSeconds();
 
     if ((hours >= 8 && minutes >= 30) && (hours < 18)) {
-        chrome.notifications.create("hydrationReminder", {
-            type: "basic",
-            iconUrl: "icons/icon-128.png",
-            title: "Time to Drink Water",
-            message: "Hydrate yourself! It's time to drink water.",
-            priority: 2,
-            requireInteraction: true
-        }, () => {
-            console.log(`Notification created at ${hours}:${minutes}:${seconds}`);
+        const notificationId = "hydrationReminder";
+
+        chrome.notifications.clear(notificationId, () => {
+            chrome.notifications.create(notificationId, {
+                type: "basic",
+                iconUrl: "icons/icon-128.png",
+                title: "Time to Drink Water",
+                message: "Hydrate yourself! It's time to drink water.",
+                priority: 2,
+                requireInteraction: true
+            }, () => {
+                console.log(`Notification created at ${hours}:${minutes}:${seconds}`);
+            });
         });
     }
 }
+
